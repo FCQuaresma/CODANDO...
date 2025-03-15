fd732b69e143a2f9a95a22ebbd1a51803242c89a
Felipe Quaresma <felipecq62@gmail.com>
2025-03-15 11:12:35 -0300

    alt exe054

--- Aprendendo Python/exe054.py
+++ Aprendendo Python/exe054.py
@@ -1,7 +1,19 @@
-n1 = int(input('Digite um numero:'))
+#n1 = int(input('Digite um numero:'))
 
-d = n1*2
-t = n1*3
-r = n1**(1/2)
+#d = n1*2
+#t = n1*3
+#r = n1**(1/2)
 
-print('o seu dobro vale {}, seu triplo vale   {} e sua raiz vale {}'.format(d, t, r))
+#print('o seu dobro vale {},\n seu triplo vale   {}\n e sua raiz vale {:.2f}'.format(d, t, r))
+
+#ouuuuuuuuuu assimmmmm
+
+#n = int(input('Digite um numero: '))
+#print('O dobro de {} vale {}.'.format(n, (n*2)))
+#print('O triplo de {} vale {} \n A raiz quadrada de {} é igual a {:.2f}.'.format(n, (n*3),n, (n**(1/2))))
+
+# ouuuuu asssimmmmmmmm
+
+n = int(input('Digite um numero: '))
+print('O dobro de {} vale {}.'.format(n, (n*2)))
+print('O triplo de {} vale {} \n A raiz quadrada de {} é igual a {:.2f}.'.format(n, (n*3),n, pow(n, 1/2)))
