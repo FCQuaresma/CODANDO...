a30776070ff721e1e4887401675e77fba9d81001
Felipe Quaresma <felipecq62@gmail.com>
2025-03-17 17:58:40 -0300

    alt exe 059

---
+++ Aprendendo Python/exe059.py
@@ -0,0 +1,11 @@
+#d = float(input('Quanto dinheiro você tem na carteira? R$:  '))
+
+#print('Com {} reais você tem {} dolares'.format(d, d/3.27) )
+
+#ouuuuuu
+
+d = float(input('Quantos reais você tem na carteira ?: R$'))
+
+dolar = d/3.27
+
+print('Com {:.2f} reais eu tenho {:.2f} dolares'.format(d, dolar))
