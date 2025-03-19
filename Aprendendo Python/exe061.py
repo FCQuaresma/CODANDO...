4850204f14c393f1180a51f9e6a73946043a1ae2
Felipe Quaresma <felipecq62@gmail.com>
2025-03-19 13:40:34 -0300

    exe061 alt

---
+++ Aprendendo Python/exe061.py
@@ -0,0 +1,15 @@
+p = float(input('Qual o preço do produto? R$:'))
+
+#d = 95/100*p
+
+#ou
+
+#d = 5/100*p
+
+#ou
+
+d = 0.95*p
+
+print('O produto que custava {:.2f}, na promoção com desconto de 5% vai custar R$: {:.2f}'.format(p, d))
+
+
