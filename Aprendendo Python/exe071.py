5d6618404c6079bbea7f044489229590b0161bae
Felipe Quaresma <felipecq62@gmail.com>
2025-03-21 21:31:53 -0300

    alt exe 071

---
+++ Aprendendo Python/exe071.py
@@ -0,0 +1,26 @@
+'''import random
+
+n1 = str(input('Primeiro Aluno: '))
+n2 = str(input('Segundo Aluno: '))
+n3 = str(input('Tercero Aluno: '))
+n4 = str(input('Quarto Aluno: '))
+
+lista = [n1, n2, n3, n4]
+
+escolhido = random.choice(lista)
+
+print('O aluno escolhido foi : {}'.format(escolhido))'''
+
+#ouuuuuuuuuuuuu
+
+from random import choice
+
+n1 = str(input('Primeiro Aluno: '))
+n2 = str(input('Segundo Aluno: '))
+n3 = str(input('Tercero Aluno: '))
+n4 = str(input('Quarto Aluno: '))
+
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+
+print('O aluno escolhido foi : {}'.format(escolhido))
