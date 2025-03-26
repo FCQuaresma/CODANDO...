9354f4e6a7dde4433bb63bf193259cacb364b78a
Felipe Quaresma <felipecq62@gmail.com>
2025-03-26 03:01:13 -0300

    alt exe 074

---
+++ Aprendendo Python/exe074.py
@@ -0,0 +1,33 @@
+frase = 'Curso em Video Python'
+
+# Imprime a frase
+
+'''print(frase)'''    
+
+# mostra do 4 indice até o 12 indice
+
+'''print(frase[3:13])'''
+
+#mostra só até o indice 12 
+
+'''print(frase[:13])'''
+
+#mostra do indice 12 até o final
+
+'''print(frase[13:])'''
+
+# mostra do indice 1 ate o 14 pulando 2 em 2 
+
+'''print(frase[1:15:2])'''
+
+# mostra desde o inicio até o  final pulando de dois em dois 
+
+'''rint(frase[::2])'''
+
+# mostrando tamanho da frase
+
+'''print(len(frase))'''
+
+# cria uma lista dividida
+
+print(frase.split())
