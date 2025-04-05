7b19d533bfea7ae98b33b4bf0ad5abc0f4ce1501
Felipe Quaresma <felipecq62@gmail.com>
2025-04-05 18:03:08 -0300

    alt exe092

---
+++ Codigos de aprendizado/Aprendendo Python/exe092.py
@@ -0,0 +1,11 @@
+# Aprendendo a colocar cores no python
+
+#    Padrao =>    '  \033[m '
+
+#print('\033[31;43m ola mundo')
+
+#print('\033[4;30;45m ola mundo\033[m')
+
+#print('\033[7;30m ola mundo\033[m')
+
+print('\033[7;33;44m ola mundo\033[m')
