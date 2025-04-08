b3441516979402f025508d0a120d68d937ee5e39
Felipe Quaresma <felipecq62@gmail.com>
2025-04-08 20:43:39 -0300

    exe 093

---
+++ Codigos de aprendizado/Aprendendo Python/exe093.py
@@ -0,0 +1,14 @@
+nome = str(input('Qual é seu nome? '))
+if nome  == 'Gustavo' :
+    print('Que belo nome!')
+
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular')
+
+elif nome in 'Ana Claudia Jessica Juliana':
+    print('Belo nome feminino')
+
+'''else:
+    print('Bem vindo ao time!')
+
+print('Tenha um bom dia, {} !'.format(nome))'''
