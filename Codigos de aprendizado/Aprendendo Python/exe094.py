ba14b40c9111dee9b9aa74d1120afd07aad95057
Felipe Quaresma <felipecq62@gmail.com>
2025-04-10 22:10:28 -0300

    alt exe094

---
+++ Codigos de aprendizado/Aprendendo Python/exe094.py
@@ -0,0 +1,14 @@
+vcasa = float(input('Qual valor da casa ? R$: '))
+sal = float(input('Qual salario do comprador? R$: '))
+financ = int(input('Quantos anos de financiamento? R$: '))
+
+prestacao = vcasa / (financ * 12)
+minimo = sal * 30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos, aprestaçao sera de R$ : {:.2f}'.format(vcasa, financ, prestacao ))
+
+#print('COMPARANDO tem que oaga {} e o minimo é {} '.format(prestacao, minimo))
+
+if prestacao <= minimo:
+    print('Emprestimo pode ser CONCEDIDO!')
+else:
+    print('Emprestimo nao pode ser CONCEDIDO!')
