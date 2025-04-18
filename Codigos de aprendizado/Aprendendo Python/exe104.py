f971fb93bb1630b26822ec39b443e1dc350b7e60
Felipe Quaresma <felipecq62@gmail.com>
2025-04-18 20:40:34 -0300

    alt exe 104

---
+++ Codigos de aprendizado/Aprendendo Python/exe104.py
@@ -0,0 +1,25 @@
+'''for c in range(0, 10, 2):
+ print(c)
+print('FIM')'''
+
+'''n = int(input(' Digite umnumero: '))
+
+for c in range(0, n):
+    print(c)
+
+print('FIM')'''
+
+'''i = int(input('inicio: '))
+f = int(input('fim: '))
+p = int(input('passo: '))
+
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')'''
+
+s = 0
+for c in range(0, 4):
+
+ n = int(input(' Digite um valor: '))
+ s += n
+print('fim')
