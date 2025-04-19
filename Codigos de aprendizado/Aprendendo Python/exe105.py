c7bed56a7b3d0da42be24a173e50c9803ef2c66a
Felipe Quaresma <felipecq62@gmail.com>
2025-04-18 21:06:09 -0300

    alt exe 105

---
+++ Codigos de aprendizado/Aprendendo Python/exe105.py
@@ -0,0 +1,9 @@
+# PROGRAMA QUE FAÇA UMA CONTAGEM REGRESSIVA E SOLTE FOGOS DE ARTIFICIOS, INDO DE 0 ATE 10, COM OAUSA DE 1 SEG.
+
+from time import sleep
+
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print(' Fiiiiiuuuuuuuu....POW....POW  !')
+
