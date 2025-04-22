cd2b205b381369ffc593ab59bb8dee9956bff559
Felipe Quaresma <felipecq62@gmail.com>
2025-04-21 21:53:13 -0300

    alt 2 exe 122

--- Codigos de aprendizado/Aprendendo Python/exe112.py
+++ Codigos de aprendizado/Aprendendo Python/exe112.py
@@ -5,3 +5,13 @@ frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split() # split() = separa as palavras.
 junto = ''.join(palavras)
 print(f' Você digitou a frase: {junto}') 
+inverso = ''
+# len() = conta o tamanho da string
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+print(f' o inverso de {junto} é {inverso}')
+print(f'{junto, inverso}')
+if inverso == junto:
+    print('A frase é um PALÍNDROMO!')
+else:
+    print('A frase NÃO é um PALÍNDROMO!')
