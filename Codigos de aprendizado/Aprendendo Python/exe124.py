600ae4540976bef177373df6cf6f835f0d4daeac
Felipe Quaresma <felipecq62@gmail.com>
2025-05-02 16:48:29 -0300

    alt 124

---
+++ Codigos de aprendizado/Aprendendo Python/exe124.py
@@ -0,0 +1,8 @@
+# CALCULANDO FATORIAL USANDO BIBLIOTECA MATH.
+
+from math import factorial
+
+n1 = int(input(' Digite um numero para calcular seu fatorial : '))
+f = factorial(n1)
+
+print(f' O fatorial de {n1} é {f}')
