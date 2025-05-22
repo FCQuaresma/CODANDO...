ef685d2850d6778597b68c9b301ad22e8eff75c5
Felipe Quaresma <felipecq62@gmail.com>
2025-05-22 18:31:50 -0300

    alt 131

---
+++ Codigos de aprendizado/Aprendendo Python/exe131.py
@@ -0,0 +1,13 @@
+s = c = s = 0
+
+
+while True:
+    n = int(input('Digite um numero: '))
+    if n == 999:
+        break
+    c += 1
+    s += n
+print(f'Foram digitados {c} numeros e a soma deles é {s}') 
+
+
+
